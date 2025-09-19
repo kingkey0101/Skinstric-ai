1ab60a0d4e6acf129b2630c200478b81aac66cbb
Keylan King <kingkey0101@outlook.com>
2025-09-19 12:07:37 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className='text-3xl font-bold text-red-600' >Hello tailwind</h3>
+    </div>
+  );
+}
+
+export default App;
