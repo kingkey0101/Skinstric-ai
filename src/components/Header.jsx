33981d58178fb794ce9bc713e84c0d749fe44c92
Keylan King <kingkey0101@outlook.com>
2025-09-19 18:36:03 -0500

    fixing SKINSTRIC button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ import btn from "../assets/button-simple.png";
 const Header = () => {
   return (
     <div className="w-1920px h-64px flex items-center m-5">
-      <a className="font-roboto font-semibold inline-flex items-center justify-center gap-2 h-9 px-4 py-2 mr-2 text-sm text-[#1A1B1C] z-1000 ">
+      <button className="font-roboto font-semibold inline-flex items-center justify-center gap-2 h-9 px-4 py-2 mr-2 text-sm text-[#1A1B1C] z-1000 ">
         SKINSTRIC
-      </a>
+      </button>
       <div className="flex items-center ml-4">
         <img src={halfRect} className="h-4" />
         <p className="m-1.5 text-[#1A1B1C] text-semibold flex items-center justify-center font-roboto opacity-70 text-xs">
