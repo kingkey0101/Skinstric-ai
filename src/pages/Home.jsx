96bb2d944b2ee8ed14578991200fa031bcf15d19
Keylan King <kingkey0101@outlook.com>
2025-09-19 23:09:55 -0500

    fixing alignment issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Rect2779 from "../assets/Rectangle 2779.svg";
+import Rect2778 from '../assets/Rectangle 2778.png';
+import btnT from '../assets/button-take-test.png'
 import btnD from "../assets/button-discover-ai.png";
 
 const Home = () => {
   return (
     <>
       <div className="relative flex h-screen w-full flex items-center gap-16">
-        {/* discover ai */}
         <div>
+          {/* discover ai */}
           <div className="flex mt-10">
             <div className="relative w-[602px] h-[602px]">
               <img src={Rect2779} alt="Rectangle-2779" />
@@ -21,6 +23,24 @@ const Home = () => {
               </button>
             </div>
           </div>
+
+          {/* take test */}
+           <div className="flex w-full justify-end mt-10">
+            <div className="relative w-[602px] h-[602px]">
+              <img src={Rect2778} alt="Rectangle-2779" />
+            
+              <button className="absolute inset-0 flex items-center pointer-events-none">
+                <img
+                  src={btnT}
+                  alt="Take Test"
+                  className="pointer-events-auto cursor-pointer"
+                />
+              </button>
+            </div>
+          </div>
+
+
+          {/* para */}
 
           <div className="mt-40 ml-10">
             <p className="text-sm font-semibold uppercase w-[316px] h-[72px]">
