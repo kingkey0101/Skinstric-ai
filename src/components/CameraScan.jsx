e54768044ccab911ee18cdee6a558aa316a81a59
Keylan King <kingkey0101@outlook.com>
2025-09-22 19:43:35 -0500

    making CameraScan.jsx

--- src/components/CameraScan.jsx
+++ src/components/CameraScan.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import scan from "../assets/AIscan.png";
+import vec1 from "../assets/Vector 1.png";
+import scan from '../assets/AIscan.png'
 import camera from "../assets/camera.png";
 import RotatingStack from "./RotatingStack";
 import { Link } from "react-router-dom";
@@ -10,19 +11,19 @@ const CameraScan = () => {
       {/* camera - left side */}
       <div className="flex items-center justify-center h-screen w-1/2">
         <div className="relative ">
-          <div className="relative transform scale-75 origin-center">
+          <div className="transform scale-50 origin-center">
             <RotatingStack />
           </div>
           <div className="absolute inset-0 flex items-center justify-center">
             <div className="relative inline-block mb-32">
               <Link>
                 <img src={camera} alt="camera" />
-              </Link>
                 <img
                   src={scan}
                   alt="scan"
-                  className="absolute -top-2 -right-2 w-10 h-10"
-                />
+                  className="absolute left-full ml-4 top-0 -translate-y-1/3 w-[200px] h-auto pointer-events-none scale-150"
+                  />
+                  </Link>
             </div>
           </div>
         </div>
